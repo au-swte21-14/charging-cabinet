d8ebb0a394a6c2f815050a9a274540a0726cb6ce
Lasse Hyldahl Jensen <lasse@lasse-it.dk>
2021-11-10 16:34:36 +0100

    Isolated StationControl tests with fakes

--- Ladeskab.Test/StationControl.Tests.cs
+++ Ladeskab.Test/StationControl.Tests.cs
@@ -3,7 +3,6 @@ using Ladeskab.Interfaces;
 using NSubstitute;
 using NSubstitute.Extensions;
 using NUnit.Framework;
-using UsbSimulator;
 
 namespace Ladeskab.Unit.Test
 {
@@ -12,7 +11,6 @@ namespace Ladeskab.Unit.Test
     {
         private IDoor _door;
         private IRfidReader _rfidReader;
-        private IUsbCharger _usbCharger;
         private IChargeControl _charger;
         private IDisplay _display;
         private ILogger _logger;
@@ -20,95 +18,93 @@ namespace Ladeskab.Unit.Test
         [SetUp]
         public void Setup()
         {
-            _usbCharger = Substitute.For<IUsbCharger>();
-            _display = new Display();
+            _display = Substitute.For<IDisplay>();
             _logger = Substitute.For<ILogger>();
-            _charger = new ChargeControl(_usbCharger, _display);
-            _door = new Door();
-            _rfidReader = new RfidReader();
+            _charger = Substitute.For<IChargeControl>();
+            _door = Substitute.For<IDoor>();
+            _rfidReader = Substitute.For<IRfidReader>();
             var unused = new StationControl(_door, _rfidReader, _charger, _display, _logger);
-
-            // When charging starts, fake 500 mA and send a event
-            _usbCharger.When(x => x.StartCharge()).Do(_ =>
-            {
-                _usbCharger.Configure().CurrentValue.Returns(500);
-                _usbCharger.CurrentValueEvent += Raise.EventWith(_usbCharger,
-                    new CurrentEventArgs {Current = _usbCharger.CurrentValue});
-            });
-            // When charging stops, fake 0 mA and send a event
-            _usbCharger.When(x => x.StopCharge()).Do(_ =>
-            {
-                _usbCharger.Configure().CurrentValue.Returns(0);
-                _usbCharger.CurrentValueEvent += Raise.EventWith(_usbCharger,
-                    new CurrentEventArgs {Current = _usbCharger.CurrentValue});
-            });
         }
-        
+
         [Test]
         public void StationOpen()
         {
-            _door.OnDoorOpen();
+            _door.OnDoorChangedEvent += Raise.EventWith(_door,
+                new DoorChangedEventArgs() {ChangeType = DoorChangedEventArgs.ChangeTypeEnum.DoorOpened});
             Assert.AreEqual("Tilslut telefon", _display.StationMessage);
-            Assert.AreEqual("", _display.ChargingMessage);
         }
-        
+
         [Test]
         public void StationClosed()
         {
-            _door.OnDoorOpen();
-            _door.OnDoorClose();
+            _door.OnDoorChangedEvent += Raise.EventWith(_door,
+                new DoorChangedEventArgs() {ChangeType = DoorChangedEventArgs.ChangeTypeEnum.DoorOpened});
+            _door.OnDoorChangedEvent += Raise.EventWith(_door,
+                new DoorChangedEventArgs() {ChangeType = DoorChangedEventArgs.ChangeTypeEnum.DoorClosed});
             Assert.AreEqual("Indlæs RFID", _display.StationMessage);
-            Assert.AreEqual("", _display.ChargingMessage);
         }
-        
+
         [Test]
         public void StationAvailableChargerNotConnected()
         {
-            _door.OnDoorOpen();
-            _door.OnDoorClose();
+            _door.OnDoorChangedEvent += Raise.EventWith(_door,
+                new DoorChangedEventArgs() {ChangeType = DoorChangedEventArgs.ChangeTypeEnum.DoorOpened});
+            _door.OnDoorChangedEvent += Raise.EventWith(_door,
+                new DoorChangedEventArgs() {ChangeType = DoorChangedEventArgs.ChangeTypeEnum.DoorClosed});
             Assert.AreEqual("Indlæs RFID", _display.StationMessage);
-            _rfidReader.OnRfidRead(1);
+            _rfidReader.OnRfidDetectedEvent += Raise.EventWith(_door, new RfidDetectedEventArgs {Id = 1});
             Assert.AreEqual("Tilslutningsfejl", _display.StationMessage);
-            Assert.AreEqual("", _display.ChargingMessage);
         }
-        
+
         [Test]
         public void StationAvailableChargerConnected()
         {
-            _usbCharger.Configure().Connected.Returns(true);
-            _rfidReader.OnRfidRead(1);
+            _charger.Configure().Connected.Returns(true);
+            _rfidReader.OnRfidDetectedEvent += Raise.EventWith(_door, new RfidDetectedEventArgs {Id = 1});
+            // This event should be ignored
+            _door.OnDoorChangedEvent += Raise.EventWith(_door,
+                new DoorChangedEventArgs() {ChangeType = DoorChangedEventArgs.ChangeTypeEnum.DoorOpened});
             Assert.AreEqual("Ladeskab optaget", _display.StationMessage);
-            Assert.AreEqual("Lader...", _display.ChargingMessage);
-            StringAssert.Contains("Skab låst med RFID", _logger.ReceivedCalls().Last().GetArguments().First().ToString());
+            StringAssert.Contains("Skab låst med RFID",
+                _logger.ReceivedCalls().Last().GetArguments().First().ToString());
+            _door.Received().LockDoor();
+            _charger.Received().StartCharge();
         }
-        
+
         [Test]
         public void StationLockedUnlock()
         {
-            _usbCharger.Configure().Connected.Returns(true);
-            _rfidReader.OnRfidRead(1);
-            _rfidReader.OnRfidRead(1);
+            _charger.Configure().Connected.Returns(true);
+            _rfidReader.OnRfidDetectedEvent += Raise.EventWith(_door, new RfidDetectedEventArgs {Id = 1});
+            _rfidReader.OnRfidDetectedEvent += Raise.EventWith(_door, new RfidDetectedEventArgs {Id = 1});
             Assert.AreEqual("Fjern telefon", _display.StationMessage);
-            Assert.AreEqual("", _display.ChargingMessage);
-            StringAssert.Contains("Skab låst op med RFID", _logger.ReceivedCalls().Last().GetArguments().First().ToString());
+            StringAssert.Contains("Skab låst op med RFID",
+                _logger.ReceivedCalls().Last().GetArguments().First().ToString());
+            _door.Received().UnlockDoor();
+            _charger.Received().StopCharge();
         }
-        
+
         [Test]
         public void StationLockedInvalidRfid()
         {
-            _usbCharger.Configure().Connected.Returns(true);
-            _rfidReader.OnRfidRead(1);
-            _rfidReader.OnRfidRead(2);
+            _charger.Configure().Connected.Returns(true);
+            _rfidReader.OnRfidDetectedEvent += Raise.EventWith(_door, new RfidDetectedEventArgs {Id = 1});
+            _rfidReader.OnRfidDetectedEvent += Raise.EventWith(_door, new RfidDetectedEventArgs {Id = 2});
             Assert.AreEqual("Forkert RFID tag", _display.StationMessage);
-            Assert.AreEqual("Lader...", _display.ChargingMessage);
+            _door.DidNotReceive().UnlockDoor();
+            _charger.DidNotReceive().StopCharge();
         }
 
         [Test]
         public void StationOpenTryRfid()
         {
-            _door.OnDoorOpen();
-            _rfidReader.OnRfidRead(1);
-            Assert.AreEqual("", _display.ChargingMessage);
+            _door.OnDoorChangedEvent += Raise.EventWith(_door,
+                new DoorChangedEventArgs() {ChangeType = DoorChangedEventArgs.ChangeTypeEnum.DoorOpened});
+            _rfidReader.OnRfidDetectedEvent += Raise.EventWith(_door, new RfidDetectedEventArgs {Id = 1});
+            _door.DidNotReceive().LockDoor();
+            _charger.DidNotReceive().StartCharge();
+            _door.DidNotReceive().UnlockDoor();
+            _charger.DidNotReceive().StopCharge();
         }
     }
 }
